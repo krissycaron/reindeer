ef29bd9cb5ec48ca802a6454abd8455a97c7e8e9
Kristin Caron <krissycaron@gmail.com>
2017-01-26 19:36:18 -0600

    started

---
+++ reindeer.js
@@ -0,0 +1,51 @@
+
+
+// loops
+
+// value of the loop i=
+
+// for(var i=0; i<5; i++){
+// 	console.log("i", i)
+// }
+
+// var fruit = ["cherries", "apples", "bananas"];
+
+//  for (var j=0; j<fruit.length; j++){
+// console.log("fruit", fruit[j]);
+
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var hohohoElement = document.getElementById("coloredReindeer");
+
+for (var i=0; i <reindeer.length, i++) {
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
